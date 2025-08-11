04dc9546ea85092b85a80b5927253cd421423971
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-11 13:05:21 +0530

    initialized react

---
+++ front-end/src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h1 class=" flex justify-center items-center text-3xl m-4 font-bold bg-blue-500 text-white p-4 rounded-xl opacity-50 hover:opacity-100 transition duration-300">
+          Hello world!
+        </h1>
+
+        <h1 class=" flex justify-center items-center  text-3xl font-bold bg-blue-900 text-white p-4 rounded-xl opacity-50 hover:opacity-100 transition duration-300">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
