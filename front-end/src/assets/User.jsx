6e53d3a9a5596d51d39ad9a5213373d08cc7548d
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-11 19:09:52 +0530

    refactor User component layout and implement user data display

--- front-end/src/assets/User.jsx
+++ front-end/src/assets/User.jsx
@@ -1,9 +1,40 @@
 import Button from 'react-bootstrap/Button';
+import React from 'react';
 
 export default function User() {
+    // Placeholder for user data, replace with actual data fetching logic
+    const [users, setUsers] = React.useState([{ name: 'John ', email: 'd3Ht6@example.com', age: 30 }]);
     return (
-        <div className="flex items-center justify-center  h-screen">
-            <h1>User</h1>
+        <div className="d-flex vh-100 align-items-center justify-content-center   h-screen bg-amber-200">
+            <div className="text-center w-50 bg-amber-500 rounded-3xl p-3">
+                <table>
+                    <thead>
+                        <tr>
+
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.age}</td>
+                                <td>
+                                    <Button variant="primary" className="m-2">Edit</Button>
+                                    <Button variant="danger" className="m-2">Delete</Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+                </table>
+
+
+            </div>
         </div>
 
 
