42b5ee30321e098b2ec59501c778f63d0fd2cd8c
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-12 01:52:17 +0530

    Adds user deletion functionality and dynamic update links
    Introduces a `handleDelete` function to enable user deletion via API calls and refreshes the page upon successful deletion. Updates the "Update" link to dynamically include the user's ID for more precise navigation. Enhances user management capabilities and improves backend integration.

--- front-end/src/assets/User.jsx
+++ front-end/src/assets/User.jsx
@@ -1,19 +1,26 @@
 import Button from 'react-bootstrap/Button';
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import CreateUser from './CreateUser';
 import axios from 'axios';
 
 
 export default function User() {
     // Placeholder for user data, replace with actual data fetching logic
-    const [users, setUsers] = React.useState([{ name: 'John ', email: 'abc@example.com', age: 30 }]);
+    const [users, setUsers] = React.useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:5000')  // Assuming you fixed your backend to handle this route
+        axios.get('http://localhost:5000')  // Fetch all users from the backend
             .then(result => setUsers(result.data))
             .catch(err => console.error(err));
     }, []);
+    const handleDelete = (id) => {
+        axios.delete(`http://localhost:5000/deleteUser/` + id)
+            .then(result => {
+                console.log(result)
+                window.location.reload();
+            })
+            .catch(err => console.error(err));
+    }
 
     return (
         <div className="min-h-screen bg-gradient-to-t from-purple-900 to-blue-900 flex justify-center items-center p-6">
@@ -35,8 +42,11 @@ export default function User() {
                                 <td className="py-3 px-4 ">{users.email}</td>
                                 <td className="py-3 px-4">{users.age}</td>
                                 <td className="py-3 px-4 text-center">
-                                    <Link to="/update-user" className="btn btn-primary m-4 rounded-pill px-4">Update</Link>
-                                    <Button variant="danger" className="m-1 rounded-pill px-4">Delete</Button>
+                                    <Link to={`/update-user/${users._id}`} className="btn btn-primary m-4 rounded-pill px-4">Update</Link>
+                                    <Button
+                                        variant="danger"
+                                        onClick={(e) => handleDelete(users._id)}
+                                        className="m-1 rounded-pill px-4">Delete</Button>
                                 </td>
                             </tr>
                         ))}
