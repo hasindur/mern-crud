cd0290a6ba4c80fd0898dd07000fb7ee0b735dec
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-11 18:20:33 +0530

    create user component

---
+++ front-end/src/assets/User.jsx
@@ -0,0 +1,16 @@
+import Button from 'react-bootstrap/Button';
+
+export default function User() {
+    return (
+        <div className="flex items-center justify-center  h-screen">
+            <div className="bg-white/20 backdrop-blur-md rounded-xl shadow-lg p-10 max-w-sm w-full text-white">
+                <h2 className="text-3xl font-bold mb-6 text-center">User Login</h2>
+
+                <p className="mt-4 text-center text-white/80 text-sm">
+                    Don't have an account? <a href="#" className="underline hover:text-white">Sign up</a>
+                </p>
+            </div>
+
+        </div >
+    );
+}
