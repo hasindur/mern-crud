21c1358c67fcd52615c088051a78461b522614b3
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-11 18:54:50 +0530

    upate user component

--- front-end/src/assets/User.jsx
+++ front-end/src/assets/User.jsx
@@ -3,29 +3,9 @@ import Button from 'react-bootstrap/Button';
 export default function User() {
     return (
         <div className="flex items-center justify-center  h-screen">
-            <div className="bg-white/20 backdrop-blur-md rounded-xl shadow-lg p-10 max-w-sm w-full text-white">
-                <h2 className="text-3xl font-bold mb-6 text-center">User Login</h2>
-                <form className="space-y-4">
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        className="form-control bg-white/30 border-0 text-black placeholder-white focus:ring-2 focus:ring-pink-400  m-2"
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        className="form-control bg-white/30 border-0 text-black placeholder-white focus:ring-2 focus:ring-pink-400  m-2"
-                    />
-                    <button type="submit" className="btn btn-primary w-100 m-2">
-                        Login
-                    </button>
+            <h1>User</h1>
+        </div>
 
-                </form>
-                <p className="mt-4 text-center text-white/80 text-sm">
-                    Don't have an account? <a href="#" className="underline hover:text-white">Sign up</a>
-                </p>
-            </div>
 
-        </div >
     );
 }
