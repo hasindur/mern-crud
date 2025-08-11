ba1c757e7dbd4e5546ecaa8ea4eb677eea0b956b
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-11 19:37:04 +0530

    feat: implement UpdateUser component with form for user details

--- front-end/src/assets/UpdateUser.jsx
+++ front-end/src/assets/UpdateUser.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 function UpdateUser() {
     return (
-        <div>UpdateUser</div>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-purple-900  to-blue-900 p-6">
+            <div className="bg-white/20 backdrop-blur-md rounded-xl shadow-lg p-10 max-w-sm w-full text-white">
+                <h2 className="text-3xl font-bold mb-6 text-center">Update User</h2>
+                <form className="space-y-4">
+                    <input
+                        type="name"
+                        placeholder="Name"
+                        className="form-control bg-white/30 border-0 text-black placeholder-white focus:ring-2 focus:ring-pink-400  m-2"
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="form-control bg-white/30 border-0 text-black placeholder-white focus:ring-2 focus:ring-pink-400  m-2"
+                    />
+                    <input
+                        type="age"
+                        placeholder="Age"
+                        className="form-control bg-white/30 border-0 text-black placeholder-white focus:ring-2 focus:ring-pink-400  m-2"
+                    />
+                    <button type="submit" className="btn btn-primary w-100 m-2">Update</button>
+
+                </form>
+
+            </div>
+
+        </div >
+
     )
 }
 
