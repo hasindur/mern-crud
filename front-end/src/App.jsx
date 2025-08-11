99590e84ea0bcd08ccd57b38a85ac8e9da9d550e
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-11 18:57:40 +0530

    refactor layout and clean up code structure

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -12,19 +12,19 @@ function App() {
 
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-purple-900  to-blue-900 p-6">
 
-        <BrowserRouter >
-          <Routes>
-            <Route path="/" element={<User />} />
-            <Route path="/create" element={<CreateUser />} />
-            <Route path="/update" element={<UpdateUser />} />
-          </Routes>
-        </BrowserRouter>
+    <div >
 
-      </div >
-    </>
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<User />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/update" element={<UpdateUser />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div >
+
   )
 }
 
